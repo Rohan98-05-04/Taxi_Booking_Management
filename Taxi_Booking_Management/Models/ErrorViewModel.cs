b34f96ed3dc8b70382e1993639ba43fb41ba9d2b
Rohan <rohanbehera6274@gmail.com>
2024-03-01 12:09:12 +0530

    Created

---
+++ Taxi_Booking_Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taxi_Booking_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
