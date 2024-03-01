43811901fbe9a112f8d441baf7b5f52ebc6be898
Anish22prakash <anish2272000@gmail.com>
2024-03-01 14:15:22 +0530

    added enum

---
+++ Taxi_Booking_Management/Common/Enums.cs
@@ -0,0 +1,40 @@
+﻿namespace Taxi_Booking_Management.Common
+{
+    public static class Enums
+    {
+        public enum Roles
+        {
+            Admin = 1,
+        }
+
+        public enum BookingStatus
+        {
+            Pending = 1,
+            Completed = 2,
+            Canceled = 3,
+        }
+
+        public enum TaxiStatus
+        {
+            Available = 1,
+            UnAvailable = 2,
+            UnderRepair = 3,
+        }
+
+        public enum TaxiType
+        {
+            Hatchback = 1,
+            Sedan = 2,
+            SUV = 3,
+            MUV =4,
+        }
+
+        public enum PaymentMedium
+        {
+            Cash = 1,
+            Online = 2,
+        }
+
+
+    }
+}
